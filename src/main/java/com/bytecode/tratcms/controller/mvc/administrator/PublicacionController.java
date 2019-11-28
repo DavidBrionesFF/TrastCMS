074ea864a613a1ef300d1d3583e842c29a3a67a9
ByteCode <aprendefacil1020@gmail.com>
2019-11-27 18:08:58 -0600

    Agregando controladores y layouts

--- src/main/java/com/bytecode/tratcms/controller/mvc/administrator/PublicacionController.java
+++ src/main/java/com/bytecode/tratcms/controller/mvc/administrator/PublicacionController.java
@@ -1,4 +1,4 @@
-package com.bytecode.tratcms.controller.mvc;
+package com.bytecode.tratcms.controller.mvc.administrator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin")
-public class AdministratorController {
-    @GetMapping("/")
+@RequestMapping("/admin/publicacion")
+public class PublicacionController {
+    @GetMapping
     public ModelAndView getHome(){
-        return new ModelAndView("administrator/admin");
+        return new ModelAndView("administrator/publicacion");
     }
 }
